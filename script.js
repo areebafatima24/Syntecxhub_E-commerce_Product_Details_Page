51b90af4a4000dc634bcea09f73a8c9f15caa9a2
areeba fatima <areebafatima720@gmail.com>
2026-02-23 03:58:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,18 @@ function addToCart() {
     btn.innerText = "Added ✓";
     btn.style.background = "#2ecc71";
 }
+
+/* Scroll Reveal */
+window.addEventListener("scroll", function() {
+    const reveals = document.querySelectorAll(".reveal");
+
+    reveals.forEach(element => {
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 100) {
+            element.classList.add("active");
+        }
+    });
+});
    
